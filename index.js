285b467c27b9d5840eec86c57b93b3c1d3f4fe95
kamil-js <kamilgrenda@icloud.com>
2022-01-28 12:48:38 +0100

    Linter le code

--- index.js
+++ index.js
@@ -2,29 +2,29 @@
  * Importation des modules nécéssaires
  */
 
-import express from 'express'
-import path from 'path'
-import {fileURLToPath} from 'url'
-import cors from 'cors'
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import cors from 'cors';
 
 /**
  * Déclaration de variables globales
  */
 
-const filename=fileURLToPath(import.meta.url)
-const dirname=path.dirname(filename)
-const port=8070
-const host='127.0.0.1'
+const filename=fileURLToPath(import.meta.url);
+const dirname=path.dirname(filename);
+const port=8070;
+const host='127.0.0.1';
 
-const app=express()
+const app=express();
 
 /**
  * Utilisation de middlewares
  */
-app.use(express.urlencoded({extended: true}))
-app.use(cors())
-app.use(express.static(path.join(dirname, 'public')))
-app.use('/favicon.ico', express.static(path.join(dirname, 'public', 'images', 'favicon.png')))
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.static(path.join(dirname, 'public')));
+app.use('/favicon.ico', express.static(path.join(dirname, 'public', 'images', 'favicon.png')));
 
 /**
  * Routage
@@ -35,17 +35,17 @@ app.use('/favicon.ico', express.static(path.join(dirname, 'public', 'images', 'f
 
 app.get('/',(req,res)=>{
   res.sendFile('index.html',{root: path.join(dirname)},err=>{
-    if (err) throw new Error(err)
-  })
-})
+    if (err) throw new Error(err);
+  });
+});
 
 app.post('/comment',(req, res)=>{
-  const comment = req.body.message
-  res.send(comment)
-})
+  const comment = req.body.message;
+  res.send(comment);
+});
 
 /**
  * Utilisation de la méthode app.listen pour écouter sur un port défini plus haut
  */
 
-app.listen(port,host,() => {console.info(`Server started @ http://${host}:${port}.`)})
+app.listen(port,host,() => {console.info(`Server started @ http://${host}:${port}.`);});
--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -5,20 +5,24 @@ const returnAnObject = (...args) => {
     args.forEach(arg => {
       response[index] = arg;
       index++;
-    })
+    });
   } else {
     response = 'No argument was given to the function.';
-  };
+  }
   return response;
 };
+
+returnAnObject();
 
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
   if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
     response = arrayOfNumbers.map((val => val * 2));
-    console.log('arrayTimesTwo: ', arrayTimesTwo);
+    console.log('arrayTimesTwo: ', response);
   } else {
     response = 'The argument is not an Array of numbers';
-  };
+  }
   return response;
 };
+
+multiplyAllByTwo();
